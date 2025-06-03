1759603ea91ff0a177008a00e06eef7368f00861
Ayub40 <ayubk4028@gmail.com>
2025-06-04 00:47:57 +0600

    added basic mongodb get,put,post,delete method

--- module-3/dist/server.js
+++ module-3/dist/server.js
@@ -12,24 +12,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mongodb_1 = require("mongodb");
 const app_1 = __importDefault(require("./app"));
-require('dotenv').config();
-const mongoose = require('mongoose');
+// import dotenv from "dotenv";
+// dotenv.config();
+const mongodb_1 = require("./config/mongodb");
+// const mongoose = require('mongoose');
 let server;
 const port = 5000;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxvdig6.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new mongodb_1.MongoClient(uri, {
-    serverApi: {
-        version: mongodb_1.ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
 const bootstrap = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield mongodb_1.client.connect();
+    console.log("Connected to MongoDB");
     server = app_1.default.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
 });
 bootstrap();
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.uxvdig6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
--- module-3/src/server.ts
+++ module-3/src/server.ts
@@ -1,28 +1,55 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+
 import app from "./app";
-require('dotenv').config();
-const mongoose = require('mongoose');
+// import dotenv from "dotenv";
+// dotenv.config();
+import { client } from "./config/mongodb";
+
+// const mongoose = require('mongoose');
 
 let server;
 const port = 5000
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uxvdig6.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
 const bootstrap = async () => {
+    await client.connect();
+    console.log("Connected to MongoDB");
     server = app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
     })
 }
 
 bootstrap()
+
+
+// ==============================================================================
+// ==============================================================================
+// ==============================================================================
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.uxvdig6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 
 
 
